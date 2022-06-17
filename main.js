1be873a6aac6144477bb2498b329475853c54ddf
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-06-17 17:18:30 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll(".like-glyph");
+const errorMessage = document.getElementById('modal');
 
-
-
+for (const like of likes) {
+  like.addEventListener("click", () => {
+    mimicServerCall("http://mimicServer.example.com")
+    .then(() => {
+      if (like.innerText === EMPTY_HEART) {
+        like.innerText = FULL_HEART;
+        like.className = "activated-heart";
+      } else {
+        like.innerText = EMPTY_HEART;
+      }
+    })
+    .catch(err => {
+      errorMessage.className = "";
+      errorMessage.innerText = err;
+      setTimeout(() => errorMessage.className = "hidden", 3000)
+    })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
